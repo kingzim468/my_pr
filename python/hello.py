a75c2d40974c0701071bafbc390b3380a3a3d5f4
kingzim468 <kingzim468@gmail.com>
2024-07-02 16:49:23 +0900

    test

---
+++ python/hello.py
@@ -0,0 +1,14 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+PORT = 9999
+
+class HandlerClass(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(f"<h3>hello</h3>".encode('utf-8'))
+
+service = HTTPServer(('', PORT), HandlerClass)
+print('HTTP Server started...[', PORT, ']')
+service.serve_forever()
